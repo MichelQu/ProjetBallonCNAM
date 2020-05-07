574a67adf017e643698b19fa3419afbf3f367cbb
michelqu <michel.qu@eleves.ec-nantes.fr>
2020-05-07 18:17:13 +0200

    Ajout d'un timer

--- Assets/Scripts/SceneP/Initialisation.cs
+++ Assets/Scripts/SceneP/Initialisation.cs
@@ -7,10 +7,22 @@ public class Initialisation : MonoBehaviour
 {
 
     public Text zoneText;
+    public Text Timer;
+    private float temps;
+    private float tempsInt;
 
     // Start is called before the first frame update
     void Start()
     {
+        temps = 0f;
         zoneText.text = "Objectif : " + PlayerPrefs.GetInt("Ballons") + " Ballons";
+        Timer.text = temps + "s";
+    }
+
+    private void Update()
+    {
+        temps += Time.deltaTime;
+        tempsInt = Mathf.RoundToInt(temps);
+        Timer.text = tempsInt + "s";
     }
 }
